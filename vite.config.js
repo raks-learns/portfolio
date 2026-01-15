ea164c512ef2f34a32a5c8182905d62404483d33
raks-learns <rakshana.ritu@gmail.com>
2026-01-15 02:20:48 +0000

    seps

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aishwarya-portfolio/", // This must match your repo name
+  base: "/portfolio/", // This must match your repo name
 })
